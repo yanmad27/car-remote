331e01b0a37002e9a1f410f6af59fc087e50ce0f
Nick <nick.phan@vegas68.com>
2023-05-19 23:05:04 +0700

    update styles

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
-import '@/components/login-form/index.css'
-import '@/components/remote/index.css'
-import '@/elements/button/button.css'
+import '@/styles/button.css'
 import '@/styles/globals.css'
+import '@/styles/login-form.css'
+import '@/styles/remote.css'
 
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />
